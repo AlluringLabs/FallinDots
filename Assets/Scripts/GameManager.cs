56fedf11d2622ff46c9e1792d297672a20a94dce
Joshua Kidd <kidd.josh.343@gmail.com>
2016-04-10 15:40:44 -0600

    Initial Commit and .gitignore additions.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+// using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	Camera mainCamera;
+	public Spawner mainSpawnPoint;
+	public Transform testSprite;
+	
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		Vector3 screenDimensions = GetScreenDimensions();
+		testSprite.position = screenDimensions;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public Vector3 GetScreenDimensions () {
+		Vector3 screenDimens = new Vector3(Screen.width, Screen.height, 0);
+		Vector3 levelDimens = mainCamera.ScreenToWorldPoint(screenDimens);
+		return levelDimens;
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	
+	public int msBetweenSpawn = 100;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void SetNewSpawnTimer(int spawnTime) {
+		
+	}
+}
