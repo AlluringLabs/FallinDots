740947b2359e52ca7969392c8948277aea616a38
David Jenkins <david.nicholas.jenkins@gmail.com>
2016-04-11 14:22:00 -0400

    Slight code cleanup

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,39 +5,46 @@ public class Spawner : MonoBehaviour
 
     public float timeBetweenSpawn = 2f;
     public Dot dot;
-	public bool isDisabled;
-	
-	float nextSpawnTime;
+    public bool isDisabled;
+
+    float nextSpawnTime;
+
 
     GameManager gameManager;
 
-    void Start() {
+    void Start()
+    {
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
-    void Update() {
-		Debug.Log("update");
-		Debug.Log("next spawn time: " + nextSpawnTime);
-		Debug.Log("time: " + Time.time);
-		// Just a basic spawn timer to spawn dots at the specified interval.
-		if (!isDisabled) {
-			if (Time.time > nextSpawnTime) {
-				
-				nextSpawnTime = Time.time + timeBetweenSpawn;
-				SpawnDot();
-			}
-		}
+    void Update()
+    {
+        Debug.Log("update");
+        Debug.Log("next spawn time: " + nextSpawnTime);
+        Debug.Log("time: " + Time.time);
+
+        // Just a basic spawn timer to spawn dots at the specified interval.
+        if (!isDisabled)
+        {
+            if (Time.time > nextSpawnTime)
+            {
+                nextSpawnTime = Time.time + timeBetweenSpawn;
+                SpawnDot();
+            }
+        }
     }
-	
-	// Creates the position and Instantiates a new dot.
-    public void SpawnDot() {
+
+    // Creates the position and Instantiates a new dot.
+    public void SpawnDot()
+    {
         Vector3 pos = gameManager.GetScreenDimensions();
 
         Instantiate(dot, pos, Quaternion.identity);
     }
 
-	// Sets a new spawn timer interval.
-    public void SetNewSpawnTimer(float newTime) {
-		timeBetweenSpawn = newTime;
+    // Sets a new spawn timer interval.
+    public void SetNewSpawnTimer(float newTime)
+    {
+        timeBetweenSpawn = newTime;
     }
 }
