9df2cebdcaae973e3302f0de1adc6ba913d411ab
Joshua Kidd <kidd.josh.343@gmail.com>
2016-04-11 11:05:24 -0600

    Added some more sprite assets and created the LevelManager.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public int currentLevel;
+	public float levelDifficulty;
+	
+	void Start () {
+		currentLevel = 0;
+		levelDifficulty = 1;
+	}
+	
+	public void Restart () {
+		currentLevel = 0;
+		levelDifficulty = 1;
+	}
+	
+	public void UpdateLevelDifficulty(int level) {
+		currentLevel = level;
+		levelDifficulty++;
+	}
+	
+}
